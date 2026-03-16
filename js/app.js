fb1ab4308d5a7fd717919f7853e944416e5a5d98
POLIHOV DEV STUDIO <polihov.alexey.a@yandex.ru>
2026-03-16 21:36:59 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1252,7 +1252,7 @@ async function sendOrder(payload) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-Order-Secret": "sytnay_dostavka_prozharim_secret_teatralnaya_liniya_kichigina_order_new"
+      "X-Order-Secret": "sytnay_dostavka_sushidza_secret_teatralnaya_liniya_kichigina_order_new"
     },
     body: JSON.stringify(payload)
   });
